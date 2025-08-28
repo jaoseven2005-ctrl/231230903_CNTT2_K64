f544c201c4d6a9646c9c6b5f47b5c0cd7df13a21
jaoseven2005 <EMAIL_BAN_DANG_KY_GITHUB>
2025-08-28 16:07:08 +0700

    First commit - Upload WebApp25

---
+++ WebApp25/Controllers/MyNewController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp25.Controllers
+{
+    public class MyNewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Sample()
+        {
+            return View();
+        }
+    }
+}
